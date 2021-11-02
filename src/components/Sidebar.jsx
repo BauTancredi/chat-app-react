b0222bf0e5b15ca26c3e185041f9be635d01f2c4
BauTancredi <bautistatancredi@hotmail.com>
2021-11-02 10:02:04 -0300

    Finished design

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SidebarChat } from "./SidebarChat";
+
+export const Sidebar = () => {
+  return (
+    <div className="inbox_chat">
+      <SidebarChat />
+
+      <div className="extra_space"></div>
+    </div>
+  );
+};
